81baccf8af6fc0d70bdd2c49ca51a5e130c1dec5
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-05-18 12:37:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,8 +16,10 @@ API_HASH = os.environ.get("API_HASH", "")
 app = Client("short_link", bot_token=TOKEN,api_hash=API_HASH,
             api_id=API_ID)
 
+
 @app.on_message(filters.command(['start']))
 def start(client, message):
+ 
  message.reply_text(text =f"Hello **{message.from_user.first_name } **\n I am simple Short link generator \n Send me long link I can convert to short \n \n**This bot created by @lntechnical** ",reply_to_message_id = message.message_id , parse_mode="markdown", reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -31,12 +33,11 @@ def echo(client, message):
  update_channel = "lntechnical"
  user_id = message.from_user.id
  if update_channel :
-   try:
-   	client.get_chat_member(update_channel, user_id)
-   except UserNotParticipant:
-   	message.reply_text("**__You are not subscribed my channel__** ",parse_mode="markdown", reply_to_message_id = message.message_id, reply_markup = InlineKeyboardMarkup([ [ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ]
-   ]))
-   return
+ 	try:
+ 		client.get_chat_member(update_channel, user_id)
+ 	except UserNotParticipant:
+ 	 message.reply_text("**__You are not subscribed my channel__** ",parse_mode="markdown", reply_to_message_id = message.message_id, reply_markup = InlineKeyboardMarkup([ [ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ]]))
+ 	 return
  msg = message.reply_text("🧐 **checking link is valid or not **",reply_to_message_id = message.message_id , parse_mode="markdown")
  link = message.text
  try:
@@ -50,4 +51,3 @@ def echo(client, message):
  msg.edit(f" ** It is your short link**\n{short_link}", disable_web_page_preview = True)
 		
 app.run()
-
