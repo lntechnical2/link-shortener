9657708cc0a2f421c9c1da1b3798c7f876d48602
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-05-18 07:07:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,8 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", 12345))
 API_HASH = API_HASH = os.environ.get("API_HASH", "")
-app = Client("short_link", bot_token =TOKEN , api_id = API_ID, api_hash = API_HASH )
+app = Client("short_link", bot_token=TOKEN,api_hash=API_HASH,
+            api_id=APP_ID)
 
 @app.on_message(filters.command(['start']))
 def start(client, message):
